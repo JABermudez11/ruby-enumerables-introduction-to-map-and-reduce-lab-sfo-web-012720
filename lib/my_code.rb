bb0b0a2c897eb2176c7a066947f08fa7353c5873
Justin Bermudez <justinanbermudez@gmail.com>
2020-01-21 10:37:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,13 @@ def map_to_negativize(array)
   end
   result
 end
+
+def map_to_no_change(array)
+  result = []
+  index = 0
+  while index < array.length do
+    result.push(array[index])
+    index += 1
+  end
+  result
+end
